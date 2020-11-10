62adea971adb93c25bf5a918da842c70d68c2b58
Home <Home@DESKTOP-Q4C5INM>
2020-11-10 15:28:14 +0200

    Addting new information in BLL and DAl

--- GroupProject/ShopWindow.xaml.cs
+++ GroupProject/ShopWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using BLL;
+using BLL.DTO;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,15 +24,55 @@ namespace GroupProject
     /// </summary>
     public partial class ShopWindow : Window
     {
+        private IBLLClass _bll = null;
+        ObservableCollection<Books> books = new ObservableCollection<Books>();
+        
         public ShopWindow()
         {
             InitializeComponent();
-            //listBox.Items.Add();
+            _bll = new BLLClass();
+
+            for (int i = 0; i < (_bll.GetAllBooks()).ToList().Count; ++i)
+            {
+                books.Add(new Books() { Author = ((_bll.GetAllBooks()).ToList()[i].Author), Name = ((_bll.GetAllBooks()).ToList()[i].Name) });
+            }
+            listBox.ItemsSource = books;
+            
         }
 
         private void btnHints_Checked(object sender, RoutedEventArgs e)
         {
             popup1.IsOpen = true;
+        }
+        public class Books
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set 
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            private string author;
+            public string Author
+            {
+                get { return author; }
+                set
+                {
+                    author = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string name = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
