80e436bf4c6cb74db4631e0611d3f1a468ef30ed
Home <Home@DESKTOP-Q4C5INM>
2020-11-11 14:59:43 +0200

    Add motivation + window motiv. + add find by name + loadind books to shop

--- GroupProject/ShopWindow.xaml.cs
+++ GroupProject/ShopWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BLL;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,15 +21,56 @@ namespace GroupProject
     /// </summary>
     public partial class ShopWindow : Window
     {
+        private IBLLClass _bll = null;
+        ObservableCollection<Books> books = new ObservableCollection<Books>();
+
         public ShopWindow()
         {
             InitializeComponent();
-            //listBox.Items.Add();
+            _bll = new BLLClass();
+
+            for (int i = 0; i < (_bll.GetAllBooks()).ToList().Count; ++i)
+            {
+                books.Add(new Books() { Author = ((_bll.GetAllBooks()).ToList()[i].Author), Name = ((_bll.GetAllBooks()).ToList()[i].Name) });
+            }
+            listBox.ItemsSource = books;
+
         }
 
         private void btnHints_Checked(object sender, RoutedEventArgs e)
         {
             popup1.IsOpen = true;
         }
+        public class Books
+        {
+            private string name;
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            private string author;
+            public string Author
+            {
+                get { return author; }
+                set
+                {
+                    author = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string name = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
     }
+}
 }
